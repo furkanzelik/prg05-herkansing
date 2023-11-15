1a44bf593ca2cd8d65ebb727911fddd35d0cd148
Huseyin Furkan Ozcelik <furkanzelik@gmail.com>
2023-11-15 15:25:59 +0100

    radiator page added

---
+++ app/Http/Controllers/RadiatorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RadiatorController extends Controller
+{
+    //
+    public function radiatorPage(){
+        return view('radiator');
+    }
+}
